18e3f525656da250774883a1dfc618ae48d1f4bf
Rodin Sarmento <73674322+Novalt@users.noreply.github.com>
2026-04-24 01:55:30 -0700

    Update IBM Cloud token placeholder in script

--- src/env-IBM-Cloud-pyAuthentication.py
+++ src/env-IBM-Cloud-pyAuthentication.py
@@ -3,7 +3,7 @@ from qiskit_ibm_runtime import QiskitRuntimeService
 # Execute esta célula UMA VEZ para salvar suas credenciais
 QiskitRuntimeService.save_account(
     channel="ibm_cloud",
-    token="15pwb8-ZqSTNA9JbTQJX1ichqRY1D07ESrFQ7TtP-hLq",  # Cole o token que você gerou
+    token="xxxxxxxxxxxxxxxxxxxxxxxx",  # Cole o token que você gerou
     instance="crn:v1:bluemix:public:quantum-computing:us-east:a/c32bc959b9024b8da06b6bbc92713040:7cfb645a-ab7c-404b-ab40-5d0dc662a74a::"
 )
 
